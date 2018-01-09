7e49382678cd9e611177e156487df88f4a4e1855
Nikita K <novembernix@gmail.com>
2018-01-09 18:29:23 +0300

    Adding all link to class

--- app/src/main/java/ru/openet/nix/opennetclient/Links.java
+++ app/src/main/java/ru/openet/nix/opennetclient/Links.java
@@ -6,5 +6,11 @@ package ru.openet.nix.opennetclient;
 
 class Links {
     final static String MAIN_NEWS_RSS_LINK = "http://www.opennet.ru/opennews/opennews_all_noadv.rss";
+    final static String MAIN_SECURITY_PROB_RSS_LINK = "http://www.opennet.ru/opennews/opennews_sec.rss";
+    final static String MAIN_NEW_SOFT_PROB_RSS_LINK = "http://www.opennet.ru/opennews/opennews_prog.rss";
+    final static String MAIN_LINUX_RSS_LINK = "http://www.opennet.ru/opennews/opennews_linux.rss";
+    final static String MAIN_BSD_RSS_LINK = "http://www.opennet.ru/opennews/opennews_bsd.rss";
     final static String UBUNTU_NEWS_RSS_LINK = "http://www.opennet.ru/opennews/opennews_ubuntu_full.rss";
+    final static String MAIN_FEDORA_RSS_LINK = "http://www.opennet.ru/opennews/opennews_fedora_full.rss";
+    final static String MAIN_MOZILLA_FIREFOX_RSS_LINK = "http://www.opennet.ru/opennews/opennews_mozilla_full.rss";
 }
